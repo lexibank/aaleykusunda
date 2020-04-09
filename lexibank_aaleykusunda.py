d38e9e313df0f4a2381999931be19ca682b7caaa
MeiShinWu <MacyL@users.noreply.github.com>
2020-04-09 13:32:59 +0200

    remove unused libraries

--- lexibank_aaleykusunda.py
+++ lexibank_aaleykusunda.py
@@ -1,14 +1,10 @@
-import re
-import csv
-import attr
 import pathlib
-import requests
+import attr
 from clldutils.misc import slug
 from pylexibank import Dataset as BaseDataset
-from pylexibank.util import progressbar as pb
-from pylexibank import Language, CONCEPTICON_CONCEPTS
-from pylexibank.forms import FormSpec
-from collections import defaultdict
+from pylexibank import progressbar as pb
+from pylexibank import Language
+from pylexibank import FormSpec
 
 
 @attr.s
@@ -21,40 +17,40 @@ class Dataset(BaseDataset):
     dir = pathlib.Path(__file__).parent
     id = "aaleykusunda"
     language_class = CustomLanguage
-    form_spec = FormSpec(
-        separators="~;,/",
-        missing_data=['∅'],
-        first_form_only=True
-    )
+    form_spec = FormSpec(separators="~;,/", missing_data=["∅"], first_form_only=True)
 
     def cmd_makecldf(self, args):
         # add bib
         args.writer.add_sources()
-        args.log.info('added sources')
+        args.log.info("added sources")
 
         # add concept
-        concepts = args.writer.add_concepts(id_factory=lambda c: c.id.split(
-            "-")[-1] + "_" + slug(c.english), lookup_factory='Name')
+        concepts = args.writer.add_concepts(
+            id_factory=lambda c: c.id.split("-")[-1] + "_" + slug(c.english),
+            lookup_factory="Name",
+        )
         # fix concept lookup
-        concepts["the barley (Tibetan or highland)"] = concepts["the barley (tibetan or highland)"]
+        concepts["the barley (Tibetan or highland)"] = concepts[
+            "the barley (tibetan or highland)"
+        ]
         concepts["to plant (vegetables, rice)"] = concepts["to plant (vegetals, rice)"]
-        args.log.info('added concepts')
+        args.log.info("added concepts")
 
         # add language
-        languages = args.writer.add_languages(lookup_factory='Name')
-        args.log.info('added languages')
+        languages = args.writer.add_languages(lookup_factory="Name")
+        args.log.info("added languages")
 
         # read in data
-        data = self.raw_dir.read_csv('Kusunda_2019_250_lexical_items.tsv',
-                                     delimiter='\t', dicts=True)
+        data = self.raw_dir.read_csv(
+            "Kusunda_2019_250_lexical_items.tsv", delimiter="\t", dicts=True
+        )
         # add data
-        for gloss, entry in pb(enumerate(data),
-                               desc='cldfify', total=len(data)):
-            if entry['ENGLISH'] in concepts.keys():
+        for entry in pb(data, desc="cldfify", total=len(data)):
+            if entry["ENGLISH"] in concepts.keys():
                 for key, val in languages.items():
                     args.writer.add_forms_from_value(
                         Language_ID=val,
-                        Parameter_ID=concepts[entry['ENGLISH']],
+                        Parameter_ID=concepts[entry["ENGLISH"]],
                         Value=entry[key],
-                        Source=['Bodt2019b']
+                        Source=["Bodt2019b"],
                     )
